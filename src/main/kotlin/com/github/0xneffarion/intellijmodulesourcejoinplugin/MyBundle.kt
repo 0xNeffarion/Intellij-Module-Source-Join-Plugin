f226c77d52187cf2d26339fac0d0bfcdfee7ca49
GitHub Action <action@github.com>
2020-12-05 23:59:47 +0000

    Template cleanup

--- src/main/kotlin/com/github/0xneffarion/intellijmodulesourcejoinplugin/MyBundle.kt
+++ src/main/kotlin/com/github/0xneffarion/intellijmodulesourcejoinplugin/MyBundle.kt
@@ -1,4 +1,4 @@
-package org.jetbrains.plugins.template
+package com.github.0xneffarion.intellijmodulesourcejoinplugin
 
 import com.intellij.AbstractBundle
 import org.jetbrains.annotations.NonNls
---
+++ src/main/kotlin/com/github/0xneffarion/intellijmodulesourcejoinplugin/services/MyApplicationService.kt
@@ -0,0 +1,10 @@
+package com.github.0xneffarion.intellijmodulesourcejoinplugin.services
+
+import com.github.0xneffarion.intellijmodulesourcejoinplugin.MyBundle
+
+class MyApplicationService {
+
+    init {
+        println(MyBundle.message("applicationService"))
+    }
+}
