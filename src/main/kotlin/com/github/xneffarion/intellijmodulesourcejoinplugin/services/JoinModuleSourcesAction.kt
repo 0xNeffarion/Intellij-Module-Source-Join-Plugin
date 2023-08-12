e263bcb2fa33c36061f46368d0039d060c68d57e
Vasco Serrano <main@neffware.com>
2023-08-12 18:49:44 +0100

    Update for IntelliJ 2023.2

--- src/main/kotlin/com/github/xneffarion/intellijmodulesourcejoinplugin/services/JoinModuleSourcesAction.kt
+++ src/main/kotlin/com/github/xneffarion/intellijmodulesourcejoinplugin/services/JoinModuleSourcesAction.kt
@@ -1,85 +1,73 @@
 package com.github.xneffarion.intellijmodulesourcejoinplugin.services
 
-import com.intellij.ide.actions.SaveAllAction
 import com.intellij.openapi.actionSystem.AnAction
 import com.intellij.openapi.actionSystem.AnActionEvent
+import com.intellij.openapi.application.ApplicationManager
 import com.intellij.openapi.module.ModuleManager
 import com.intellij.openapi.project.Project
-import com.intellij.openapi.project.ProjectManager
-import com.intellij.openapi.project.guessProjectDir
-import com.intellij.openapi.vfs.LocalFileSystem
-import java.io.File
-import java.io.FileNotFoundException
+import com.intellij.openapi.project.guessModuleDir
+import com.intellij.openapi.ui.Messages
+import com.intellij.openapi.vfs.*
 import java.io.IOException
-import java.nio.file.CopyOption
 import java.nio.file.Files
+import java.nio.file.StandardCopyOption
 
 
 class JoinModuleSourcesAction : AnAction() {
 
     override fun actionPerformed(e: AnActionEvent) {
-        SaveAllAction().actionPerformed(e)
-        val project: Project = e.project!!
-        val folderName = "joined"
-        val projectDir = project.guessProjectDir() ?: return
-
-        val joinDir = projectDir.path + "/" + folderName
-        val joinDirFile = File(joinDir)
-        if (!joinDirFile.exists()) {
-            Files.createDirectory(joinDirFile.toPath())
-        } else {
-            deleteDir(joinDirFile)
-            Files.createDirectory(joinDirFile.toPath())
+        val project: Project? = e.project
+        if (project == null) {
+            Messages.showErrorDialog("No project found.", "Error")
+            return
         }
 
-        for (module in ModuleManager.getInstance(project).modules) {
-            val moduleFile = module.moduleFilePath
-            val index = moduleFile.indexOfLast { c -> c == '/' }
-            val dir = moduleFile.substring(0, index + 1) + "src"
-            copyFolder(File(dir), joinDirFile)
-        }
+        val baseDir = project.projectFile?.parent?.parent!!;
 
-        LocalFileSystem.getInstance().refresh(true)
-    }
+        ApplicationManager.getApplication().runWriteAction {
 
-    @Throws(IOException::class)
-    fun copyFileOrFolder(source: File, dest: File, vararg options: CopyOption?) {
-        if (source.isDirectory) {
-            copyFolder(source, dest, *options)
-        } else {
-            ensureParentFolder(dest)
-            copyFile(source, dest, *options)
-        }
-    }
-
-    @Throws(IOException::class)
-    private fun copyFolder(source: File, dest: File, vararg options: CopyOption?) {
-        if (!dest.exists()) dest.mkdirs()
-        val contents = source.listFiles()
-        if (contents != null) {
-            for (f in contents) {
-                val newFile = File(dest.absolutePath + File.separator + f.name)
-                if (f.isDirectory) copyFolder(f, newFile, *options) else copyFile(f, newFile, *options)
+            var joinedDir = baseDir.findChild("joined");
+            if (joinedDir?.exists()!!){
+                Files.walk(joinedDir.toNioPath())
+                        .sorted(Comparator.reverseOrder())
+                        .forEach { path ->
+                            Files.delete(path)
+                        }
+            }else{
+                joinedDir = baseDir.findOrCreateDirectory("joined");
             }
+
+            for (module in ModuleManager.getInstance(project).modules) {
+                val moduleDir = module.guessModuleDir()!!
+                val src = moduleDir.findChild("src")!!
+                copyFolder(src, joinedDir)
+            }
+
+            LocalFileSystem.getInstance().refresh(false)
+        }
+
+        Messages.showInfoMessage("Module sources joined.", "Success")
+    }
+
+    private fun copyFolder(source: VirtualFile, destination: VirtualFile) {
+        try {
+            Files.walk(source.toNioPath()).use { stream ->
+                stream.forEach { sourcePath ->
+                    val sourcePathIo = source.toNioPath();
+                    val destinationPathIo = destination.toNioPath();
+
+                    val relativePath = sourcePathIo.relativize(sourcePath)
+                    val destinationPath = destinationPathIo.resolve(relativePath)
+
+                    if (Files.isDirectory(sourcePath)) {
+                        Files.createDirectories(destinationPath)
+                    } else {
+                        Files.copy(sourcePath, destinationPath, StandardCopyOption.REPLACE_EXISTING)
+                    }
+                }
+            }
+        } catch (e: IOException) {
+            println("An error occurred: ${e.message}")
         }
     }
-
-    @Throws(IOException::class)
-    private fun copyFile(source: File, dest: File, vararg options: CopyOption?) {
-        Files.copy(source.toPath(), dest.toPath(), *options)
-    }
-
-    private fun ensureParentFolder(file: File) {
-        val parent = file.parentFile
-        if (parent != null && !parent.exists()) parent.mkdirs()
-    }
-
-    @Throws(IOException::class)
-    fun deleteDir(f: File) {
-        if (f.isDirectory) {
-            for (c in f.listFiles()) deleteDir(c)
-        }
-        if (!f.delete()) throw FileNotFoundException("Failed to delete file: $f")
-    }
-
 }
